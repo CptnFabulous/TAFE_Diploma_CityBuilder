c13fe4694773da605e5629066bfc22b60fa89bdf
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-09-23 16:05:02 +1000

    update

---
+++ Assets/Martin/Scripts/BuildingStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingStats : MonoBehaviour
+{
+    public GameObject buildingmanagement;
+
+    private void Start()
+    {
+        buildingmanagement = GameObject.FindGameObjectWithTag("BuildingManagement");
+    }
+}
