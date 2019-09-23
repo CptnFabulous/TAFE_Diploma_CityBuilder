e6721ab90e305c091956ccea85b7fdca8e5b7e9b
Marcus Mouyat <marcus@mouyat.com>
2019-09-23 14:24:22 +1000

    Made date/time code, camera code, and resource manager script

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource
+{
+    int current;
+    int max;
+}
+
+public class ResourceManager : MonoBehaviour
+{
+
+
+
+    public int money;
+    public int income;
+    public int spending;
+    public bool inDebt;
+
+    public int energyGeneration;
+    public int energyConsumption;
+
+    public int population;
+    [Range(0, 100)]
+    public float happiness;
+    
+    bool InDebt()
+    {
+        if (money > 0)
+        {
+            return false;
+        }
+
+        // Do stuff when in debt
+        return true;
+    }
+
+    bool SpendMoney(int moneyToSpend)
+    {
+        if (money >= moneyToSpend)
+        {
+            money -= moneyToSpend;
+            return true;
+        }
+        return false;
+    }
+
+    void SpendBudget()
+    {
+        money -= spending;
+    }
+
+    void Income(int income)
+    {
+        money += income;
+    }
+}
