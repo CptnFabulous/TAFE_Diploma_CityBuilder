4ace98175d02f192e1b2fa2a555a3b95f8b2ea56
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-09-23 11:49:55 +1000

    building variables

---
+++ Assets/Martin/Scripts/Buildings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Buildings
+{
+    [System.Serializable]
+    public struct buildings
+    {
+        public string name;
+        public int purchaseCost;
+        public int energyProduction;
+        public int energyUsage;
+        public int maintenanceCost;
+        public int income;
+        public int population;
+        public int happiness;
+        public BuildingTypes type;
+    };
+}
+public enum BuildingTypes
+{
+    Power,
+    Shop,
+    Housing,
+    Entertainment
+}
